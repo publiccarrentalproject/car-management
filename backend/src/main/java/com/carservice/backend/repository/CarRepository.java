c720a4d1c431dbde78633e97bbd80123db93dd59
mehmetmgrsl <mehmetgursul@gmail.com>
2020-03-31 01:18:00 +0200

    Dockerized the application

--- backend/src/main/java/com/carservice/backend/repository/CarRepository.java
+++ backend/src/main/java/com/carservice/backend/repository/CarRepository.java
@@ -7,8 +7,5 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import com.carservice.backend.model.Car;
 
 public interface CarRepository extends MongoRepository<Car, String> {
-
 	List<Car> findByRentable(boolean isRentable);
-
-	Car findByPlateNumber(String id);
 }
