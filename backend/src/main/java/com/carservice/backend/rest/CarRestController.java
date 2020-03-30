5492f697baef855d6874a31843ce9c66781f681a
mehmetmgrsl <mehmetgursul@gmail.com>
2020-03-31 01:18:00 +0200

    Dockerized the application

--- backend/src/main/java/com/carservice/backend/rest/CarRestController.java
+++ backend/src/main/java/com/carservice/backend/rest/CarRestController.java
@@ -1,16 +1,13 @@
 package com.carservice.backend.rest;
 
-import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.carservice.backend.model.Car;
 import com.carservice.backend.repository.CarRepository;
@@ -22,18 +19,19 @@ public class CarRestController {
 	private CarRepository carRepository;
 
 	@GetMapping(value = "/")
-	public ResponseEntity<List<Car>> getRentableCars() {
-		List<Car> result = carRepository.findByRentable(true);
+	public ResponseEntity<List<Car>> getCars(@RequestParam("rentable") boolean rentable) {
+		List<Car> result = carRepository.findByRentable(rentable);
 		
 		return ResponseEntity.ok(result);
 	}
 
-	@PostMapping(value = "/car")
-	public ResponseEntity<URI> createCar(@RequestBody Car car) {
-		carRepository.save(car);
-		URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{plateNumber}").buildAndExpand(car.getPlateNumber()).toUri();
-		
-		return ResponseEntity.created(location).build();
-	}
+//	@PostMapping(value = "/car")
+//	public ResponseEntity<URI> createCar(@RequestBody Car car) {
+//		carRepository.save(car);
+//		
+//		URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{plateNumber}").buildAndExpand(car.getPlateNumber()).toUri();
+//		
+//		return ResponseEntity.created(location).build();
+//	}
 
 }
