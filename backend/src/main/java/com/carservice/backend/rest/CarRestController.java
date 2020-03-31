1f3e5db98a74002a8f9eebe9e3d85b95bac24f2b
mehmetmgrsl <mehmetgursul@gmail.com>
2020-03-31 22:46:03 +0200

    Rest controller was updated

--- backend/src/main/java/com/carservice/backend/rest/CarRestController.java
+++ backend/src/main/java/com/carservice/backend/rest/CarRestController.java
@@ -1,37 +1,48 @@
 package com.carservice.backend.rest;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.carservice.backend.model.Car;
 import com.carservice.backend.repository.CarRepository;
 
 @RestController
-@RequestMapping("/cars")
+@RequestMapping("/rest")
 public class CarRestController {
 	@Autowired
 	private CarRepository carRepository;
 
-	@GetMapping(value = "/")
+	@GetMapping(value = "/cars")
 	public ResponseEntity<List<Car>> getCars(@RequestParam("rentable") boolean rentable) {
 		List<Car> result = carRepository.findByRentable(rentable);
-		
+
 		return ResponseEntity.ok(result);
 	}
 
-//	@PostMapping(value = "/car")
-//	public ResponseEntity<URI> createCar(@RequestBody Car car) {
-//		carRepository.save(car);
-//		
-//		URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{plateNumber}").buildAndExpand(car.getPlateNumber()).toUri();
-//		
-//		return ResponseEntity.created(location).build();
-//	}
+	@PostMapping(value = "/car")
+	public ResponseEntity<URI> createCar(@RequestBody Car car) {
+		try {
+			carRepository.save(car);
+
+			URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{plateNumber}")
+					.buildAndExpand(car.getPlateNumber()).toUri();
+
+			return ResponseEntity.created(location).build();
+
+		} catch (Exception ex) {
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+		}
+	}
 
 }
