b43dd5f5f70c0a2849849d7d683d42e9fec00c27
mehmetmgrsl <mehmetgursul@gmail.com>
2020-04-05 13:05:10 +0200

    PUT method was added REST API

--- backend/src/main/java/com/carservice/backend/repository/CarRepository.java
+++ backend/src/main/java/com/carservice/backend/repository/CarRepository.java
@@ -8,7 +8,9 @@ import com.carservice.backend.model.Car;
 
 public interface CarRepository extends MongoRepository<Car, String> {
 	List<Car> findByRentable(boolean isRentable);
+
 	Car findByPlateNumber(String plateNumber);
+
 	void deleteByPlateNumber(String plateNumber);
-	
+
 }
