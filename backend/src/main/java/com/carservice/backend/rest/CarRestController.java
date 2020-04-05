7c9eed737fdbca8e5531ff7958a43b0f168a8685
mehmetmgrsl <mehmetgursul@gmail.com>
2020-04-05 13:05:21 +0200

    PUT method was added REST API

--- backend/src/main/java/com/carservice/backend/rest/CarRestController.java
+++ backend/src/main/java/com/carservice/backend/rest/CarRestController.java
@@ -3,6 +3,7 @@ package com.carservice.backend.rest;
 import java.net.URI;
 import java.util.List;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -31,14 +33,13 @@ public class CarRestController {
 
 		return ResponseEntity.ok(result);
 	}
-	
+
 	@GetMapping(value = "/car/{plateNumber}")
 	public ResponseEntity<Car> getCars(@PathVariable("plateNumber") String plateNumber) {
 		Car result = carRepository.findByPlateNumber(plateNumber);
 
 		return ResponseEntity.ok(result);
 	}
-	
 
 	@PostMapping(value = "/car")
 	public ResponseEntity<URI> createCar(@RequestBody Car car) {
@@ -54,11 +55,20 @@ public class CarRestController {
 			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
 		}
 	}
-	
-    @DeleteMapping("/car/{plateNumber}")
-    public void deleteByPlateNumber(@PathVariable("plateNumber") String plateNumber){
-        this.carRepository.deleteByPlateNumber(plateNumber);
-    }
 
+	@PutMapping(value = "/car/{plateNumber}")
+	public ResponseEntity<?> updateOwner(@PathVariable("plateNumber") String plateNumber, @RequestBody Car aCar) {
+		System.out.println(aCar);
+		Car car = carRepository.findByPlateNumber(plateNumber);
+		BeanUtils.copyProperties(aCar, car, "id");
+		System.out.println(car);
+		carRepository.save(car);
+		return ResponseEntity.ok().build();
+	}
+
+	@DeleteMapping("/car/{plateNumber}")
+	public void deleteByPlateNumber(@PathVariable("plateNumber") String plateNumber) {
+		this.carRepository.deleteByPlateNumber(plateNumber);
+	}
 
 }
