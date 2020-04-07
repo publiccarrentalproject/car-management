59cd41ebca768147ba8ddb72d4eae565090f8da0
mehmetmgrsl <mehmetgursul@gmail.com>
2020-04-07 23:20:33 +0200

    Service layer and exception handling was added

--- backend/src/main/java/com/carservice/backend/rest/CarRestController.java
+++ backend/src/main/java/com/carservice/backend/rest/CarRestController.java
@@ -1,95 +1,87 @@
 package com.carservice.backend.rest;
 
-import java.net.URI;
-import java.util.List;
-
+import com.carservice.backend.model.Car;
+import com.carservice.backend.service.CarService;
+import com.carservice.exception.CarNotFoundException;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import com.carservice.backend.model.Car;
-import com.carservice.backend.repository.CarRepository;
+import java.net.URI;
+import java.util.List;
 
 @RestController
 @RequestMapping("/rest")
 public class CarRestController {
-	@Autowired
-	private CarRepository carRepository;
+    @Autowired
+    private CarService carService;
 
-	@GetMapping(value = "/cars")
-	public ResponseEntity<List<Car>> getCars(@RequestParam("rentable") boolean rentable) {
-		List<Car> result = carRepository.findByRentable(rentable);
+    @GetMapping(value = "/cars")
+    public ResponseEntity<List<Car>> getCars(@RequestParam("rentable") boolean rentable) {
+        List<Car> result = carService.findByRentable(rentable);
 
-		return ResponseEntity.ok(result);
-	}
+        return ResponseEntity.ok(result);
+    }
 
-	@GetMapping(value = "/car/{plateNumber}")
-	public ResponseEntity<Car> getCars(@PathVariable("plateNumber") String plateNumber) {
-		Car result = carRepository.findByPlateNumber(plateNumber);
+    @GetMapping(value = "/car/{plateNumber}")
+    public ResponseEntity<Car> getCars(@PathVariable("plateNumber") String plateNumber) {
+        try {
+            Car car = carService.findByPlateNumber(plateNumber);
+            return ResponseEntity.ok(car);
+        } catch (CarNotFoundException ex) {
+            return ResponseEntity.notFound().build();
+        }
+    }
 
-		if (result == null) {
-			return ResponseEntity.notFound().build();
-		}
+    @PostMapping(value = "/car")
+    public ResponseEntity<URI> createCar(@RequestBody Car car) {
+        try {
+            carService.save(car);
 
-		return ResponseEntity.ok(result);
-	}
+            URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{plateNumber}")
+                    .buildAndExpand(car.getPlateNumber()).toUri();
 
-	@PostMapping(value = "/car")
-	public ResponseEntity<URI> createCar(@RequestBody Car car) {
-		try {
-			carRepository.save(car);
+            return ResponseEntity.created(location).build();
 
-			URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{plateNumber}")
-					.buildAndExpand(car.getPlateNumber()).toUri();
+        } catch (Exception ex) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
 
-			return ResponseEntity.created(location).build();
+    @PutMapping(value = "/car/{plateNumber}")
+    public ResponseEntity<?> updateOwner(@PathVariable("plateNumber") String plateNumber, @RequestBody Car aCar) {
+        try {
 
-		} catch (Exception ex) {
-			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
-		}
-	}
+            Car car = carService.findByPlateNumber(plateNumber);
 
-	@PutMapping(value = "/car/{plateNumber}")
-	public ResponseEntity<?> updateOwner(@PathVariable("plateNumber") String plateNumber, @RequestBody Car aCar) {
-		Car car = carRepository.findByPlateNumber(plateNumber);
+            BeanUtils.copyProperties(aCar, car, "id");
 
-		if (car == null) {
-			return ResponseEntity.notFound().build();
-		}
+            carService.save(car);
 
-		BeanUtils.copyProperties(aCar, car, "id");
+            return ResponseEntity.ok().build();
 
-		try {
-			carRepository.save(car);
-		} catch (Exception ex) {
-			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
-		}
+        } catch (CarNotFoundException ex) {
+            return ResponseEntity.notFound().build();
+        } catch (Exception ex) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
 
-		return ResponseEntity.ok().build();
-	}
+    @DeleteMapping("/car/{plateNumber}")
+    public ResponseEntity<?> deleteByPlateNumber(@PathVariable("plateNumber") String plateNumber) {
+        try {
+            carService.findByPlateNumber(plateNumber);
 
-	@DeleteMapping("/car/{plateNumber}")
-	public ResponseEntity<?> deleteByPlateNumber(@PathVariable("plateNumber") String plateNumber) {
-		Car car = carRepository.findByPlateNumber(plateNumber);
+            this.carService.deleteByPlateNumber(plateNumber);
 
-		if (car == null) {
-			return ResponseEntity.notFound().build();
-		}
+            return ResponseEntity.ok().build();
 
-		this.carRepository.deleteByPlateNumber(plateNumber);
-		
-		return ResponseEntity.ok().build();
-	}
+        } catch (CarNotFoundException ex) {
+            return ResponseEntity.notFound().build();
+        }
+    }
 
 }
