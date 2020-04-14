441d9e00a569410020fe032ba1893cfe59d2c6bf
mehmetmgrsl <mehmetgursul@gmail.com>
2020-04-14 23:18:39 +0200

    test data was updated

--- backend/src/test/java/com/carservice/backend/rest/DataProvider.java
+++ backend/src/test/java/com/carservice/backend/rest/DataProvider.java
@@ -18,24 +18,39 @@ public class DataProvider {
         this.carRepository = carRepository;
     }
 
-    public void createUsers() {
-        Car car = new Car();
-        car.setPlateNumber("EE-4321-EE");
-        car.setBrand("Toyota");
-        car.setModel("Corolla");
-        car.setYear(2011);
-        car.setEngine("D-4D");
-        car.setFuelType("Diesel");
-        car.setFuelConsumption(6);
-        car.setNumberOfDoors(4);
-        car.setNumberOfSeats(5);
-        car.setRentable(true);
+    public void createCars() {
+        Car car1 = new Car();
+        car1.setPlateNumber("EE-4321-EE");
+        car1.setBrand("Toyota");
+        car1.setModel("Corolla");
+        car1.setYear(2011);
+        car1.setEngine("D-4D");
+        car1.setFuelType("Diesel");
+        car1.setFuelConsumption(6);
+        car1.setNumberOfDoors(4);
+        car1.setNumberOfSeats(5);
+        car1.setRentable(true);
 
-        carRepository.save(car);
+        carRepository.save(car1);
+
+        Car car2 = new Car();
+        car2.setPlateNumber("WW-9876-WW");
+        car2.setBrand("Toyota");
+        car2.setModel("Yaris");
+        car2.setYear(2015);
+        car2.setEngine("1.1");
+        car2.setFuelType("Gasoline");
+        car2.setFuelConsumption(4);
+        car2.setNumberOfDoors(4);
+        car2.setNumberOfSeats(5);
+        car2.setRentable(true);
+
+        carRepository.save(car2);
     }
 
     public void deleteUsers() {
         carRepository.deleteByPlateNumber("EE-4321-EE");
+        carRepository.deleteByPlateNumber("WW-9876-WW");
         carRepository.deleteByPlateNumber("DD-4321-DD");
     }
 }
