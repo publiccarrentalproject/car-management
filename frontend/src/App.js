b8185f42b63b27d3d2433e7af5c95fdc48e618c5
MehmetGursul <mehmetgursul@gmail.com>
2020-05-16 20:35:49 +0200

    Car Admin Gui components were created

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,12 @@
 import React from 'react';
 
 import CarAdminComponent from './components/CarAdminComponent';
-
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-
       <CarAdminComponent />
-
     </div>
   );
 }
--- frontend/src/components/FooterComponent.jsx
+++ frontend/src/components/FooterComponent.jsx
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
+import AppBar from '@material-ui/core/AppBar'
+import Toolbar from '@material-ui/core/Toolbar'
+import Typography from '@material-ui/core/Typography'
+
 
 
 class FooterComponent extends Component {
     render() {
-        return (
-            "Footer Component"
-        )
+        return (<AppBar position="static">
+            <Toolbar>
+                <Typography variant="h6">
+                    Car Service Admin Page
+                </Typography>
+            </Toolbar>
+        </AppBar>)
     }
 }
 
