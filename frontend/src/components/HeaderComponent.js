0b2d08aeb4b0be8813afdeae9b2d3ec66e75292a
MehmetGursul <mehmetgursul@gmail.com>
2020-05-17 15:29:08 +0200

    ui components were refactored

--- frontend/src/components/HeaderComponent.js
+++ frontend/src/components/HeaderComponent.js
@@ -10,7 +10,7 @@ class HeaderComponent extends Component {
         return (<AppBar position="static">
             <Toolbar>
                 <Typography variant="h6">
-                    Car Service Admin Page
+                    Car Service > Admin Page
                 </Typography>
             </Toolbar>
         </AppBar>)
