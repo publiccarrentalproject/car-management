7d78e9695df5f0de41c26615a8ce39f828007e0d
MehmetGursul <mehmetgursul@gmail.com>
2020-05-17 15:29:08 +0200

    ui components were refactored

--- frontend/src/components/AddDeleteButtonsComponent.jsx
+++ frontend/src/components/AddDeleteButtonsComponent.jsx
@@ -5,20 +5,19 @@ import Button from '@material-ui/core/Button'
 class AddDeleteButtonsComponent extends Component {
     render() {
         return (
-            <form noValidate autoComplete="off">
-                <Grid container spacing={3}>
-                    <Grid item xs={3} spacing={1}>
-                        <Button variant="outlined" color="primary">
-                            Add New Car
-                        </Button>
-                    </Grid>
-                    <Grid item xs={3} spacing={1}>
-                        <Button variant="outlined" color="primary">
-                            Delete Car(s)
-                        </Button>
-                    </Grid>
+            <Grid container spacing={3}>
+                <Grid item xs={2} spacing={1}>
+                    <Button variant="outlined" color="primary">
+                        Delete Car(s)
+                    </Button>
                 </Grid>
-            </form >
+
+                <Grid item xs={7} spacing={1}>
+                    <Button variant="outlined" color="primary">
+                        Add New Car
+                    </Button>
+                </Grid>
+            </Grid>
         )
     }
 }
